1754994b22090872740b007a44290a7607355f12
Trevor Williams <tmw7991@gmail.com>
2024-02-15 16:31:19 -0700

    feat: account for spans in text reveal

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ function initAnimations(): void {
 
   elements.forEach((element) => {
     if (element instanceof HTMLElement) {
+      element.style.visibility = 'visible';
       const animation = element.getAttribute('data-ld-animation');
       if (!animation) return;
       if (animation === 'text-reveal') handleTextAnimation(element);
@@ -65,21 +66,40 @@ function handleDefaultAnimation(element: HTMLElement, animation: string): void {
 }
 
 function handleTextAnimation(element: HTMLElement) {
-  const lines = element.innerText.split('\n');
+  const delay = parseFloat(element.getAttribute('data-ld-delay') || '0');
+  const delayInSeconds = delay * 0.001;
+  let stagger = parseFloat(element.getAttribute('data-ld-stagger') || '0');
+
+  const lines = element.innerHTML.split('\n');
 
   element.innerHTML = lines
     .map((line) => {
-      const words = line.split(' ');
+      // TODO: create a function or use a library
+      line = line.replace(/&nbsp;/g, ' ');
 
-      return words
-        .map((word) => {
-          const letters = word
-            .split('')
-            .map((letter) => `<span class="letter">${letter}</span>`)
-            .join('');
-          return `<span class="word">${letters}</span>`;
-        })
-        .join(' ');
+      const htmlString = line.replace(
+        /(<span[^>]*>[\s\S]*?<\/span>)|([\w'’]+|[^\w\s<]+)/g,
+        (match, p1, p2) => {
+          if (p1) {
+            return p1.replace(/class="/, 'class="word ');
+          } else if (p2) {
+            return `<span class="word">${p2}</span>`;
+          }
+          return match;
+        }
+      );
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(htmlString, 'text/html');
+
+      const words = doc.querySelectorAll('.word');
+      words.forEach((word) => {
+        const wordText = word.textContent || '';
+        const letters = [...wordText]; // Using spread operator to correctly handle characters
+        word.innerHTML = letters.map((letter) => `<span class="letter">${letter}</span>`).join('');
+      });
+
+      return doc.body.innerHTML;
     })
     .join('<br>');
 
@@ -88,18 +108,31 @@ function handleTextAnimation(element: HTMLElement) {
       entries.forEach((entry) => {
         const element = entry.target as HTMLElement;
         const letters = element.querySelectorAll('.letter');
-        const staggerMax = 0.02;
-        const staggerMin = 0.005;
-        const stagger = Math.min(staggerMax, Math.max(staggerMin, 1 / letters.length));
+        if (stagger === 0) {
+          const staggerMax = 0.05;
+          const staggerMin = 0.005;
+          stagger = Math.min(staggerMax, Math.max(staggerMin, 1 / letters.length));
+        }
+
+        const yOffset = '30%';
+        const duration = 0.2;
 
         if (entry.isIntersecting) {
           gsap.fromTo(
             letters,
-            { y: '10px', opacity: 0 },
-            { y: '0', opacity: 1, duration: 0.2, stagger, ease: 'power3.out' }
+            { y: yOffset, opacity: 0 },
+            {
+              y: '0',
+              opacity: 1,
+              duration,
+              stagger,
+              ease: 'power3.out',
+              delay: delayInSeconds,
+            }
           );
         } else {
-          gsap.set(letters, { y: '10px', opacity: 0 });
+          gsap.killTweensOf(letters);
+          gsap.set(letters, { y: yOffset, opacity: 0 });
         }
       });
     },
@@ -155,12 +188,15 @@ function initObserver(element: HTMLElement, animationInstance: GSAPAnimation) {
           element.style.visibility = 'visible';
           animationInstance.play();
           observer.unobserve(element);
+        } else {
+          animationInstance.pause();
+          animationInstance.progress(0);
         }
       });
     },
     {
       root: null,
-      rootMargin: '0% 0px -50% 0px',
+      rootMargin: '-40% 0px -50% 0px',
       threshold: 0.01,
     }
   );
